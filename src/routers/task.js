4dcb867d5857ce5f9f9b9f3e04209148c63e26a7
Nitish Kumar <krnitish@live.in>
2021-08-10 23:18:48 +0530

    Comment added

--- src/routers/task.js
+++ src/routers/task.js
@@ -80,6 +80,7 @@ router.patch('/tasks/:id', auth, async (req, res) => {
 
 router.delete('/tasks/:id', auth, async (req, res) => {
     try {
+        // Delete any task with ID
         const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
         if (!task) {
             return res.status(404).send()
